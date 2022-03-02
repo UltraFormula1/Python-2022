ad88b14d7a1a5584b9bb671784d80ff8e0ec1461
UltraFormula1 <100097049+UltraFormula1@users.noreply.github.com>
2022-03-03 12:36:07 +1300

    Create 5_input.py
    not done yet

---
+++ 5_input.py
@@ -0,0 +1,52 @@
+#1
+input("Type in any key and press enter")
+#2
+input("Type in your name")
+#3
+input("Now type in your age")
+#4
+name = input("Type in your name")
+#5
+age = input("Now type in your age")
+#6
+movie = input("What is your favorite movie?")
+#7
+book = input("What is your favorite book?")
+#8
+adjective = input("Type in an adjective")
+#9
+noun = input("Type in a noun")
+#10
+verb = input("type in a verb")
+#11
+print(f"Hello {name}, I see you're {age} years old, good to meet you!")
+print(f"{movie} is a good movie, but I don't think that {book} is a good read, however, I do recommend another book by the same author")
+print(f"Your adjective is {adjective}, your noun is {noun} and your verb is {verb}")
+#12
+user_age = int(input("I need your age again please"))
+#13
+print(f"You will be {user_age +10} in 10 years")
+#14
+print(f"You were born in {2022 - user_age}")
+#15
+apples = int(input("How many apples do you have?"))
+#16
+friends = int(input("If you aren't a lonely loser, tell me how many friends you have?"))
+#17
+print(f"You can share {apples//friends} apples between your mates, and have{apples%friends} left over.")
+#18
+pizza = int(input("How many pizza's would you like?"))
+#19
+people = int(input("How many people want pizza?"))
+#20
+print(f"you're friends will each get {pizza*8//friends} and have {pizza*8%friends} remaining")
+#21
+dollars = int(input("How much money do you have?"))
+#22
+TV = int(input("How much is the TV"))
+#23
+print(f"You will have {dollars - TV} left after purchasing the TV")
+#24
+print(f"The TV will be {0.8*TV} while on a 20% off sale")
+#25
+bitcoin_amount = int(input("How much Bitcoin do you have?"))
