a6f4ed3b1be337775eb577b25e34bee0ad5ad63d
UltraFormula1 <100097049+UltraFormula1@users.noreply.github.com>
2022-03-09 11:00:40 +1300

    Create 8_lists.py
    bruh sus

---
+++ 8_lists.py
@@ -0,0 +1,81 @@
+#1
+Fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+print(Fibonacci)
+#2
+fruit = ['apple', 'banana', 'mandarin', 'grape', 'watermelon']
+print(fruit)
+#3
+Youtubers = ['videogamedunkey', 'Steven He', 'The Detail', 'AntDude', 'The Act Man', 'Scott The Woz']
+print(Youtubers)
+#4
+songs = []
+songs.append('Michael Jackson - Bad')
+songs.append('The Beatles - Get Back')
+songs.append('Michael Jackson - Smooth Criminal')
+songs.append('Gorillaz - Feel Good Inc.')
+songs.append('The Beatles - Abbey Road Medley')
+print(songs)
+#5
+books = []
+books.append(input("Type in a book"))
+books.append(input("Type in a 2nd book"))
+books.append(input("Type in a 3rd book"))
+books.append(input("Type in a 4th book"))
+books.append(input("Type in a 5th book"))
+books.sort()
+print(books)
+#6
+pizza_toppings = []
+pizza_toppings.append(input("Type in a pizza topping and press enter"))
+if pizza_toppings[-1] == "":
+	pizza_toppings.pop()
+pizza_toppings.append(input("Type in a pizza topping and press enter"))
+if pizza_toppings[-1] == "":
+	pizza_toppings.pop()
+pizza_toppings.append(input("Type in a pizza topping and press enter"))
+if pizza_toppings[-1] == "":
+	pizza_toppings.pop()
+pizza_toppings.append(input("Type in a pizza topping and press enter"))
+if pizza_toppings[-1] == "":
+	pizza_toppings.pop()
+pizza_toppings.append(input("Type in a pizza topping and press enter"))
+if pizza_toppings[-1] == "":
+	pizza_toppings.pop()
+pizza_toppings.append(input("Type in a pizza topping and press enter"))
+if pizza_toppings[-1] == "":
+	pizza_toppings.pop()
+print(pizza_toppings)
+#7
+fruits = ['apple', 'banana', 'mandarin', 'grape', 'watermelon']
+fruits.append(input("Type in a fruit"))
+if fruits[-1] == "":
+	fruits.pop()
+	print("That is already in the list, dumbass")
+fruits.append(input("Type in a fruit"))
+if fruits[-1] == "":
+	fruits.pop()
+	print("That is already in the list, dumbass")
+fruits.append(input("Type in a fruit"))
+if fruits[-1] == "":
+	fruits.pop()
+	print("That is already in the list, dumbass")
+fruits.append(input("Type in a fruit"))
+if fruits[-1] == "":
+	fruits.pop()
+	print("That is already in the list, dumbass")
+fruits.append(input("Type in a fruit"))
+if fruits[-1] == "":
+	fruits.pop()
+	print("That is already in the list, dumbass")
+fruits.append(input("Type in a fruit"))
+if fruits[-1] == "":
+	fruits.pop()
+	print("That is already in the list, dumbass")
+print(fruits)
+#8
+names = ['John', 'Paul', 'George', 'Ringo', 'Michael']
+names.sort()
+print(names)
+names.reverse()
+print(names)
+#9
